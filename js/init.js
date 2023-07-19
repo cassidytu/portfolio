4a3fa6288b14d775be8ecd9ffaa74eb7d506570c
cassidytu <cassidytu@ucla.edu>
2023-07-18 20:51:50 -0700

    removed second project under see more footer

--- js/init.js
+++ js/init.js
@@ -132,22 +132,26 @@ window.onscroll = function(ev) {
   });
 })(window, document);
 
-//dropdown button in nav bar
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+function onLinkClick() {
+  document.getElementsById('works').scrollIntoView();
+};
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
+// //dropdown button in nav bar
+// /* When the user clicks on the button, 
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//   var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }
 
 // hover effect for happy face
 var image = document.getElementById("happyFace");
